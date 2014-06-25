6253698113fa047e592304df7b577da28aef2120
Tijl Houtbeckers <tijl@splendo.nl>
2014-06-25 15:03:43 +0200

    Allow title string without ID

--- Injctr/src/main/java/houtbecke/rs/injctr/Title.java
+++ Injctr/src/main/java/houtbecke/rs/injctr/Title.java
@@ -9,7 +9,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({ ElementType.TYPE })
 public @interface Title {
-    int value();
+    int value() default 0;
     String string() default "";
 
 
