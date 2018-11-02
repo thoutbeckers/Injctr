e12ed8e985959021207aadfaf8a72ec466f80c57
Gijs van Veen <daeda88@gmail.com>
2018-11-02 17:48:07 +0100

    Move to AndroidX

--- Injctr/src/main/java/houtbecke/rs/injctr/InjctrUtil.java
+++ Injctr/src/main/java/houtbecke/rs/injctr/InjctrUtil.java
@@ -7,7 +7,7 @@ import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
--- Injctr/src/main/java/houtbecke/rs/injctr/base/InjctrDialogFragment.java
+++ Injctr/src/main/java/houtbecke/rs/injctr/base/InjctrDialogFragment.java
@@ -3,7 +3,7 @@ package houtbecke.rs.injctr.base;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
